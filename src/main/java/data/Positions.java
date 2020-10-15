7edf16923fec4d8d3e23b4ba5f75ce16b10bfa0a
kekzdealer <kekzdealer@gmail.com>
2020-10-15 12:25:00 +0200

    and finally the new code

---
+++ src/main/java/data/Positions.java
@@ -0,0 +1,43 @@
+package data;
+
+import java.nio.ByteBuffer;
+
+public class Positions {
+
+    public static final int BYTE_COUNT = 12;
+
+    public int id;
+
+    public int x;
+
+    public int y;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public byte[] bytes() {
+        return ByteBuffer.allocate(BYTE_COUNT).putInt(id).putInt(x).putInt(y).array();
+    }
+
+}
