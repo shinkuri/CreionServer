c507d14724c9db50ec1c274140d8e11dcaa48647
Kekzdealer <kekzdealer@gmail.com>
2020-10-29 22:08:04 +0100

    can now receive packets and print the data as string

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,21 @@
+import net.LoginReceptor;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.config.Configurator;
+
+import java.net.SocketException;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        Configurator.setAllLevels(LogManager.getRootLogger().getName(), Level.INFO);
+
+        try {
+            final LoginReceptor loginReceptor = new LoginReceptor(666, 10000);
+            loginReceptor.start();
+        } catch (SocketException socketException) {
+            socketException.printStackTrace();
+        }
     }
 }
--- src/main/java/net/LoginReceptor.java
+++ src/main/java/net/LoginReceptor.java
@@ -1,26 +1,40 @@
+package net;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
+import java.net.SocketTimeoutException;
 
 public class LoginReceptor extends Thread {
 
+    private static final Logger LOGGER = LogManager.getLogger();
+
     private final DatagramSocket socket;
 
-    public LoginReceptor(int port) throws SocketException {
+    public LoginReceptor(int port, int timeout) throws SocketException {
         socket = new DatagramSocket(port);
+        socket.setSoTimeout(timeout);
     }
 
     @Override
     public void run() {
         while(!isInterrupted()) {
             try {
+                LOGGER.log(Level.INFO, "Waiting for package on " + socket.getLocalPort());
+
                 final DatagramPacket p = new DatagramPacket(new byte[512], 512);
                 socket.receive(p);
 
                 final String s = new String(p.getData(), p.getOffset(), p.getLength());
+                LOGGER.log(Level.INFO, s);
 
-
+            } catch (SocketTimeoutException socketTimeoutException) {
+                LOGGER.log(Level.INFO, "Timed out!");
             } catch (IOException e) {
                 e.printStackTrace();
             }
